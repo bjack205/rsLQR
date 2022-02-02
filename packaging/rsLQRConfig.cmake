625fc9a7f3922ca5d31bcc4ec9e154be934ac272
Brian Jackson <bjack205@gmail.com>
2022-02-02 15:28:07 -0500

    Export eigen

--- packaging/rsLQRConfig.cmake
+++ packaging/rsLQRConfig.cmake
@@ -1,3 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/rsLQR.cmake)
 find_package(OpenMP REQUIRED)
+<<<<<<< HEAD
 find_package(Eigen3 3.3 NO_MODULE)
+=======
+find_package(Eigen3 3.3 NO_MODULE)
+>>>>>>> 0fd678c (Export eigen)
