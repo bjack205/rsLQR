e05b0eaf01963a54eca3e1b6d9ef86764093fb17
Brian Jackson <bjack205@gmail.com>
2022-02-01 22:16:44 -0500

    Add comments back

--- deploydocs.sh
+++ deploydocs.sh
@@ -6,13 +6,20 @@ CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "Deploying to gh-pages branch"
 git config user.email "bjack205@gmail.com"
 git config user.name "Brian Jackson"
+echo "Fetch"
 git fetch
+echo "Checkout"
 git checkout gh-pages || exit 1
+echo "Copy"
 cp -r build/docs/html/* html || exit 1
 sleep 0.5
+echo "Add"
 git add html/* || exit 1
+echo "Commit"
 git commit -m "Update documentation" || exit 1
+echo "Push"
 git push -f origin gh-pages || exit 1
+echo "Checkout"
 git checkout $CUR_BRANCH || exit 1
 
 cd $CUR_DIR
