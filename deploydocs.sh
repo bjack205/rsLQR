ad80aada7e116ffe79fd7ad2125818844108f1ed
Brian Jackson <bjack205@gmail.com>
2022-02-01 22:10:01 -0500

    Get rid of print statements

--- deploydocs.sh
+++ deploydocs.sh
@@ -6,17 +6,12 @@ CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "Deploying to gh-pages branch"
 git config user.email "bjack205@gmail.com"
 git config user.name "Brian Jackson"
-echo $PWD
 git fetch
-git checkout origin/gh-pages && exit 1
-echo "Copying html files"
+git checkout gh-pages && exit 1
 cp -r build/docs/html/* html && exit 1
 sleep 0.5
-echo "Adding html folder"
 git add html/* && exit 1
-echo "Committing changes"
 git commit -m "Update documentation" && exit 1
-echo "pushing to origin"
 git push -f origin gh-pages && exit 1
 git checkout $CUR_BRANCH && exit 1
 
