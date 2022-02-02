6c7f9614cc9fdc965b3050dd1dbbf4398fedb8d6
Brian Jackson <bjack205@gmail.com>
2022-02-01 22:11:55 -0500

    Flip error logic

--- deploydocs.sh
+++ deploydocs.sh
@@ -7,13 +7,13 @@ echo "Deploying to gh-pages branch"
 git config user.email "bjack205@gmail.com"
 git config user.name "Brian Jackson"
 git fetch
-git checkout gh-pages && exit 1
-cp -r build/docs/html/* html && exit 1
+git checkout gh-pages || exit 1
+cp -r build/docs/html/* html || exit 1
 sleep 0.5
-git add html/* && exit 1
-git commit -m "Update documentation" && exit 1
-git push -f origin gh-pages && exit 1
-git checkout $CUR_BRANCH && exit 1
+git add html/* || exit 1
+git commit -m "Update documentation" || exit 1
+git push -f origin gh-pages || exit 1
+git checkout $CUR_BRANCH || exit 1
 
 cd $CUR_DIR
 
