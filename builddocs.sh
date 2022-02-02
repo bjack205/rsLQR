302079157e0670d35528216f872bb3c73f7604ad
Brian Jackson <bjack205@gmail.com>
2022-02-01 22:30:11 -0500

    Build documentation on CI (#6)

---
+++ builddocs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CUR_DIR=$PWD
+
+cd $SCRIPT_DIR
+
+if [ ! -d "build" ]; then
+    mkdir build
+    cd build
+    cmake ..
+else
+    cd build
+fi
+
+echo "Building the documentation"
+cmake ..
+cmake --build . --target doxygen
+
+cd $CUR_DIR
